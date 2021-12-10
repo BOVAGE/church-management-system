185a793803e390d07a657a2eeac952e610860766
bovage <fayemibolu2@gmail.com>
2021-12-10 08:13:08 -1200

    remove local db

--- church/settings.py
+++ church/settings.py
@@ -93,6 +93,7 @@ WSGI_APPLICATION = 'church.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -103,7 +104,7 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
