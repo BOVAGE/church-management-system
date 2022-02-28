62e3b4f8b636c49e78a0bf7b7500c439616f6880
bovage <fayemibolu2@gmail.com>
2022-02-27 23:22:17 -1200

    style: remove var static_root repeated twice

--- church/settings.py
+++ church/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' no longer using whitenoise
 
 MEDIA_URL = '/media/'
