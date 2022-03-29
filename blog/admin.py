f27e89faa92cd7c9f1031ab441905e78ce08aca9
bovage <fayemibolu2@gmail.com>
2022-03-28 21:08:27 -1200

    feat: customize post model in admin site

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models import Announcement, Category, Comment, Post
 
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'date_created']
+    list_filter = ['title', 'author', 'date_created']
+    search_fields = ['title', 'body', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Category)
 admin.site.register(Announcement)
