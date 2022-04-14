d85fe04cf469c58f5dacb0d5f4aa68c29bf4d2e4
bovage <fayemibolu2@gmail.com>
2022-04-13 17:45:43 -1200

    chore: comment system of blog post

--- blog/admin.py
+++ blog/admin.py
@@ -8,8 +8,12 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'body', 'description']
     prepopulated_fields = {'slug': ('title',)}
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'comment_text', 'post']
+    search_fields = ['user__username', 'comment_text', 'post__title']
+    list_filter = ['user', 'post']
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Category)
 admin.site.register(Announcement)
