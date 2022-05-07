1493206daf8a3911fb87fea39b2ee67e5443832e
bovage <fayemibolu2@gmail.com>
2022-05-06 23:26:51 -1200

    feat: add custom template tag - show_latest_sermons

---
+++ sermon/templatetags/sermon_tags.py
@@ -0,0 +1,10 @@
+from atexit import register
+from django import template
+from ..models import Sermon
+
+register = template.Library()
+
+@register.inclusion_tag('sermon/latest_sermons.html')
+def show_latest_sermons(count=4):
+    latest_sermons = Sermon.objects.all()[:count]
+    return {'latest_sermons': latest_sermons}
