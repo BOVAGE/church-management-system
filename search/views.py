fd8b1f5750352eebdcf0d0655ebbea1875b7ab7c
bovage <fayemibolu2@gmail.com>
2022-05-12 05:18:31 -1200

    feat: add search stemming and ranking

--- search/views.py
+++ search/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from blog.models import Post
 from sermon.models import Sermon
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery,SearchRank
 from itertools import chain
 from django.contrib.auth import get_user_model
 
 # Create your views here.
 def search(request):
     query = request.GET.get('q')
-    post_search = Post.objects.filter(body__search=query)
-    sermon_search = Sermon.objects.filter(content__search=query)
+    search_query = SearchQuery(query)
+    post_search_vector = SearchVector('title', 'body')
+    sermon_search_vector = SearchVector('title', 'content')
+    post_search = Post.objects.annotate(search=post_search_vector, \
+    rank=SearchRank(post_search_vector, search_query))\
+    .filter(search=search_query).order_by('-rank')
+    sermon_search = Sermon.objects.annotate(search=sermon_search_vector,\
+    rank=SearchRank(sermon_search_vector, search_query))\
+    .filter(search=search_query).order_by('-rank')
     r = chain(post_search, sermon_search)
     result = list(r)
     context = {'search_result': result, 'query': query}
